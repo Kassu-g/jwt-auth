52d2d1e53a083a890b14d7d298f94d467bbca941
kasper666g <64731263+kasper666g@users.noreply.github.com>
2024-12-16 23:13:04 +0200

    sss

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,28 +7,65 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcryptjs');
-const auth = express();
-auth.use(bodyParser.json());
-const p = 3000;
-const kayt = [];
-const fs = require('fs');
-auth.post('/api/user/register', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { email, password } = req.body;
-    if (kayt.find(user => user.email === email)) {
-        return res.status(403).json({ error: 'Email already in use' });
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-    const salattu = yield bcrypt.hash(password, 6);
-    const uusi = { email, password: salattu };
-    kayt.push(uusi);
-    fs.writeFileSync('./results.txt', JSON.stringify(kayt, null, 2));
-    res.status(201).json(uusi);
-}));
-auth.get('/api/user/list', (req, res) => {
+};
+var _this = this;
+var express = require('express');
+var bodyParser = require('body-parser');
+var bcrypt = require('bcryptjs');
+var auth = express();
+auth.use(bodyParser.json());
+var p = 3000;
+var kayt = [];
+var fs = require('fs');
+auth.post('/api/user/register', function (req, res) { return __awaiter(_this, void 0, void 0, function () {
+    var _a, email, password, salattu, uusi;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _a = req.body, email = _a.email, password = _a.password;
+                if (kayt.find(function (user) { return user.email === email; })) {
+                    return [2 /*return*/, res.status(403).json({ error: 'Email already in use' })];
+                }
+                return [4 /*yield*/, bcrypt.hash(password, 6)];
+            case 1:
+                salattu = _b.sent();
+                uusi = { email: email, password: salattu };
+                kayt.push(uusi);
+                fs.writeFileSync('./results.txt', JSON.stringify(kayt, null, 2));
+                res.status(201).json(uusi);
+                return [2 /*return*/];
+        }
+    });
+}); });
+auth.get('/api/user/list', function (req, res) {
     res.status(200).json(kayt);
 });
-auth.listen(p, () => {
-    console.log(`toimii: http://localhost:${p}`);
+auth.listen(p, function () {
+    console.log("toimii: http://localhost:".concat(p));
 });
